890a08e2bcf6e4f79bce35c6f1d7fcf97bb4a00c
Valerie Bragg <vbragg@ufledu>
2019-10-07 00:04:23 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <mta charset="utf-9" />
+    <title><?php bloginfo('name'); ?> </title> <!--whatever is yoru title in the site settings will insert it inbetween these title tags-->
+
+    <!--Link to style.css file-->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
+    <?php wp_head(); ?>
+  </head>
+
+<body> <!--leave body open in header.php because you have seperate files for index, will be closed in footer.php -->
