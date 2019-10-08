4fb1a241b7e112f342fe37773f9ba948ae15105f
Valerie Bragg <vbragg@ufledu>
2019-10-07 20:51:43 -0400

    Minor edit

--- functions.php
+++ functions.php
@@ -8,10 +8,10 @@ ADD STYLESHEET AND JAVASCRIPT create_function
 ****************************************************************************************/
 
 
-function custom_theme_scripts() {
+function custom_theme_scripts(){
 
 //bootstrap integration
-wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+//wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
 
 //main css file
 wp_enqueue_style('main-styles', get_stylesheet_uri());
