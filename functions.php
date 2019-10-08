a6d8948c1acd6bb7d94f27e7bbbb9c34e5176e49
Valerie Bragg <vbragg@ufledu>
2019-10-07 20:43:39 -0400

    Add functions to get stylesheets

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/*****************************************************************************************
+****************************************************************************************
+
+ADD STYLESHEET AND JAVASCRIPT create_function
+
+*****************************************************************************************
+****************************************************************************************/
+
+
+function custom_theme_scripts() {
+
+//bootstrap integration
+wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css');
+
+//main css file
+wp_enqueue_style('main-styles', get_stylesheet_uri());
+
+
+}
+
+add_action('wp_enqueue_scripts','custom_theme_scripts');
+
+ ?>
