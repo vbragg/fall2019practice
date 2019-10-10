af3a8f8174ca20c148556d79ee62a87a03e60e06
Valerie Bragg <vbragg@ufledu>
2019-10-09 22:17:44 -0400

    Add another post

--- index.php
+++ index.php
@@ -5,13 +5,10 @@
     if(have_posts()){
       while(have_posts()){
         the_post(); ?>
-        <div class="col-md-3">
+        <div class="col-md-4">
           <h3><?php the_title(); ?></h3>
 
           <p><?php echo "Published: " . get_the_date(); echo " | "; echo "Written by: " . get_the_author();?></p>
-
-          <?php the_date(); ?>
-          <?php the_author(); ?>
 
           <div class="post-featured-image">
             <?php the_post_thumbnail('thumbnail'); ?>
