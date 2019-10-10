b8e48ac58e83a2cd3fbca80a27edc7e019fb4618
Valerie Bragg <vbragg@ufledu>
2019-10-10 00:39:34 -0400

    Add get home url to header

--- single.php
+++ single.php
@@ -10,6 +10,11 @@
 
             <h2 class="entry-title"><?php the_title(); ?></h2>
 
+            <div class="">
+              <?php the_post_thumbnail('large'); ?>
+
+            </div>
+
             <p class="post-information"><?php echo "<span class='bold'>Published </span>" . get_the_date(); echo " | <span class= 'bold'>Written by: </span>" . get_the_author(); ?></p>
 
             <div class = "content">
