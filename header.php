ea68173e992dce02fab3f9328cb91e4f20c693ea
Valerie Bragg <vbragg@ufledu>
2019-10-10 00:00:17 -0400

    Test link for homepage

--- header.php
+++ header.php
@@ -4,9 +4,6 @@
     <mta charset="utf-9" />
     <title><?php bloginfo('name'); ?> </title> <!--whatever is yoru title in the site settings will insert it inbetween these title tags-->
 
-    <!--Link to bootstrap
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
--->
     <!--Link to Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap" rel="stylesheet">
 
@@ -19,7 +16,7 @@
 <body> <!--leave body open in header.php because you have seperate files for index, will be closed in footer.php -->
   <header class-"container">
     <div class="mx-auto py-4" style="width: 250px;">
-      <h1><a href = "../../../../index.php">Hello World!</a></h1>
+      <h1><a href = "www.valeriebragg.com/wordpress">Hello World!</a></h1>
     </div>
 
   </header>
