bac6b0fc85c26dd1b83c779aeefca882c52f2d3c
Valerie Bragg <vbragg@ufledu>
2019-10-09 22:45:32 -0400

    Move thumbnail images to the bottom

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
 
           <p><?php echo "Published: " . get_the_date(); echo " | "; echo "Written by: " . get_the_author();?></p>
 
-          <div class="post-featured-image">
-            <?php the_post_thumbnail('thumbnail'); ?>
-          </div>
           <?php the_excerpt(); ?>
 
 
           <a class="btn btn-primary btn-sm" href="<?php the_permalink(); ?>">Read More</a>
+
+          <div class="post-featured-image">
+            <?php the_post_thumbnail('thumbnail'); ?>
+          </div>
         </div>
 
     <?php  }// this is the end of the while loop
