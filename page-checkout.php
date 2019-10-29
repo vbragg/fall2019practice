c67cb3ea23bb4f9ac428f56e6c287e78230ef04c
Valerie Bragg <vbragg@ufledu>
2019-10-28 20:27:14 -0400

    Add page-checkout.php

---
+++ page-checkout.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: Checkout Page Template
+Template Post Type: page
+
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <main class = "twelve columns single-content">
+      <?php
+        if(have_posts()){
+          while(have_posts()){
+            the_post(); ?>
+
+            <h2 class="entry-title"><?php the_title(); ?></h2>
+
+            <?php the_content(); ?>
+        <?php  }//ends while loop
+        }//ends th if statement
+       ?>
+    </main>
+  </div>
+</div>
+<?php get_footer(); ?>
