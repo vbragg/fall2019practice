94ca00e3280e18c71d2fad1f369e0a49ceda968f
Valerie Bragg <vbragg@ufledu>
2019-10-28 20:40:47 -0400

    Edit styling code on page-checkout

--- page-checkout.php
+++ page-checkout.php
@@ -10,18 +10,20 @@ Template Post Type: page
 
 <div class="container">
   <div class="row">
-    <div class = "twelve columns single-content">
-      <?php
-        if(have_posts()){
-          while(have_posts()){
-            the_post(); ?>
+    <div class = "col-lg-12">
+      <main>
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post(); ?>
 
-            <h2 class="entry-title"><?php the_title(); ?></h2>
+              <h2 class="entry-title"><?php the_title(); ?></h2>
 
-            <?php the_content(); ?>
-        <?php  }//ends while loop
-        }//ends th if statement
-       ?>
+              <?php the_content(); ?>
+          <?php  }//ends while loop
+          }//ends th if statement
+         ?>
+     </main>
     </div>
   </div>
 </div>
