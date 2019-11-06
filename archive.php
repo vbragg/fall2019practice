255d2cc7d460a4409261e6a5ea4eee60176d9dd3
Valerie Bragg <vbragg@ufledu>
2019-11-05 20:56:42 -0500

    Add Read More permalink to archive page

--- archive.php
+++ archive.php
@@ -5,14 +5,17 @@ get_header();
 
 <div class="container">
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-12 individual-post">
       <main>
       <?php if(have_posts()) {
         while(have_posts()){
           the_post();?>
           <div class="col-md-12">
             <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-            <p><?php the_time('F j, Y'); ?></p>
+            <p>Published on: <?php the_time('F j, Y'); ?></p>
+            <p><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink() ?>">Read More...</a>
+
           </div>
           <?php
         } //ends while loop
