f167913ea40deb2c02f0e443b877792b50c3d89a
Valerie Bragg <vbragg@ufledu>
2019-11-05 21:06:07 -0500

    Put individual-post class on main tag

--- archive.php
+++ archive.php
@@ -5,8 +5,8 @@ get_header();
 
 <div class="container">
   <div class="row">
-    <div class="col-md-12 individual-post">
-      <main>
+    <div class="col-md-12">
+      <main class="individual-post">
       <?php if(have_posts()) {
         while(have_posts()){
           the_post();?>
