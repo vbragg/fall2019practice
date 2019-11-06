97c373f5a6b156ad89c524e704b44eef858ab3dd
Valerie Bragg <vbragg@ufledu>
2019-11-05 22:59:48 -0500

    Add title and content to search page

--- search.php
+++ search.php
@@ -1,13 +1,16 @@
-<?php /*Template Name: Search pagw */
+<?php /*Template Name: Search page */
  get_header(); ?>
 
 <div class="container">
   <div class="row">
       <div class="col-md-12">
         <?php if(have_posts()){?>
-          <h1><?php printf(__('Search Results for : %s'), '<span>' . get_search_query() . '</span>')?></h1>
+          <h1><?php printf(__('Search Results for : %s'), '<span>' . get_search_query() . '</span>');?></h1>
           <?php while(have_posts()){
-            the_post();
+            the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content();
+
           } //Ends while loop
         } //Ends if statement
       ?>
