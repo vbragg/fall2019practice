d47bbedacb686f054a5fd8d0165a80f0fae866ad
Valerie Bragg <vbragg@ufledu>
2019-11-05 21:21:52 -0500

    Add daily, monthly, yearly archieves

--- archive.php
+++ archive.php
@@ -1,4 +1,4 @@
-<?php /*Template Name: Archieve Page */
+<?php /*Template Name: Archive Page */
 
 get_header();
 ?>
@@ -6,6 +6,22 @@ get_header();
 <div class="container">
   <div class="row">
       <div class="col-md-12">
+        <h2><?php
+          if(is_category()) {
+            single_cat_title();
+          }elseif(is_tag()) {
+            single_tag_title();
+          }elseif(is_day()){
+            echo "Daily Archives: " . get_the_date();
+          }elseif(is_month()){
+            echo "Monthly Archives: " . get_the_date('F Y');
+          }elseif(is_year()){
+            echo "Yearly Archives: " . get_the)date ('Y');
+          }else{
+            echo "Archives";
+          }
+         ?>
+       </h2>
       <?php if(have_posts()) {
         while(have_posts()){
           the_post();?>
