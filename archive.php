b149ce4bcc03c2ef0d7b5581a87784a09bfb6a7b
Valerie Bragg <vbragg@ufledu>
2019-11-05 21:14:11 -0500

    -

--- archive.php
+++ archive.php
@@ -5,11 +5,11 @@ get_header();
 
 <div class="container">
   <div class="row">
-      <div class="col-md-12 individual-post">
+      <div class="col-md-12">
       <?php if(have_posts()) {
         while(have_posts()){
           the_post();?>
-          <div class="col-md-12">
+          <div class="col-md-12 individual-post">
             <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
             <p>Published on: <?php the_time('F j, Y'); ?></p>
             <p><?php the_excerpt(); ?></p>
