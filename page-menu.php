8664ddc71ab68d046994ea867f42d99d277638e6
Valerie Bragg <vbragg@ufledu>
2019-11-12 21:39:15 -0500

    Add bootstrap styling to page-menu.php

--- page-menu.php
+++ page-menu.php
@@ -17,37 +17,39 @@ Template Post Type: page
 
 
       while($loop->have_posts()): $loop->the_post();   //to display loop ?>
+      <div class="row">
 
-      <div class="col-md-5">
-        <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+        <div class="col-lg-5">
+          <h3><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
 
-        <?php if(has_post_thumbnail()){
-          the_post_thumbnail('medium');
-        }?>
-      </div>
-
-      <div class="col-md-7">
-        <div class="ingredients">
-          <h3>Ingredients</h3>
-          <?php the_field('ingredients'); ?>
+          <?php if(has_post_thumbnail()){
+            the_post_thumbnail('medium');
+          }?>
         </div>
 
-        <div class="price">
-          <h3>Price</h3>
-          <?php the_field('price'); ?>
-        </div>
+        <div class="col-lg-7">
+            <div class="ingredients">
+              <h3>Ingredients</h3>
+              <?php the_field('ingredients'); ?>
+            </div>
 
-        <div class="size">
-          <h3>Size</h3>
-          <?php the_field('size'); ?>
-        </div>
+            <div class="price">
+              <h3>Price</h3>
+              <?php the_field('price'); ?>
+            </div>
 
-        <div class="vegan">
-          <h3>Vegan available?</h3>
-          <?php the_field('vegan'); ?>
+            <div class="size">
+              <h3>Size</h3>
+              <?php the_field('size'); ?>
+            </div>
+
+            <div class="vegan">
+              <h3>Vegan available?</h3>
+              <?php the_field('vegan'); ?>
+            </div>
+            <a href="<?php the_permalink();?>">Read More>></a>
         </div>
-        <a href="<?php the_permalink();?>">Read More>></a>
-      </div>
+    </div>
 
 
     <?php endwhile;?>
