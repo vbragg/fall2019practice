13683ed94e102d56034241f25e65ae358d6ef344
Valerie Bragg <vbragg@ufledu>
2019-11-18 21:12:16 -0500

    Remove ; from comments page

--- comments.php
+++ comments.php
@@ -5,7 +5,7 @@ foreach($comments as $comment):
   echo get_avatar($comment, 32);
   echo '</div>';
 
-  echo '<div class="comment-author">'; . get_comment_author_link()
+  echo '<div class="comment-author">' . get_comment_author_link()
   . '</div>';
 
   echo 'div class="comment-date">' . get_comment_date() .
