ad4a5df9b3970afff846b3baeaf378bfa69aab1b
Valerie Bragg <vbragg@ufledu>
2019-11-18 20:56:06 -0500

    Add comments page

---
+++ comments.php
@@ -0,0 +1,19 @@
+<?php
+
+foreach($comments as $comment):
+  echo '<div class="comment-avatar">';
+  echo get_avatar($comment, 32);
+  echo '</div>';
+
+  echo '<div class="comment-author">'; . get_comment_author_link()
+  . '</div>';
+
+  echo 'div class="comment-date">' . get_comment_date() .
+  '</div>';
+
+  echo 'div class="comment-text">' . get_comment_text() .
+  '</div>';
+
+endforeach;
+
+?>
