c8a58131b1c7715bb2faa60063419a1957d81463
Valerie Bragg <vbragg@ufledu>
2019-11-18 21:18:24 -0500

    Correct brackets around divs in comments page:

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <?php
 
-foreach($comments as $comment):
+foreach ($comments as $comment):
   echo '<div class="comment-avatar">';
   echo get_avatar($comment, 32);
   echo '</div>';
@@ -8,10 +8,10 @@ foreach($comments as $comment):
   echo '<div class="comment-author">' . get_comment_author_link()
   . '</div>';
 
-  echo 'div class="comment-date">' . get_comment_date() .
+  echo '<div class="comment-date">' . get_comment_date() .
   '</div>';
 
-  echo 'div class="comment-text">' . get_comment_text() .
+  echo '<div class="comment-text">' . get_comment_text() .
   '</div>';
 
 endforeach;
